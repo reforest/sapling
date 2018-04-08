2bb137f2fcf233b169840a289dc46a606fa49518
Nuno Neves <youknownuno@gmail.com>
2018-04-07 21:32:12 -0700

    making trees

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const { Scene } = require('./framework/Scene.js');
 const THREE = require('three');
 const { objects } = require('./framework/objects.js');
-const { materials } = require('./framework/materials.js');
+const { materials, greenShades } = require('./framework/materials.js');
+const { createShape } = require('./framework/constructors.js');
 
 //imports the setup from stage.js
 let scene = new Scene();
@@ -18,11 +19,28 @@ const treeMaker = function(x,z) {
 		for (var j = 0; j < z; j++) {
 
 			let tree = new THREE.Group();
-			
 			let trunk = objects.makeTrunk(materials.bone, 0, 50, 0);
 			tree.add(trunk);
-			let leaves = objects.makeLeaves(materials.celadonGreen, 0, 100, 0);
-			tree.add(leaves);
+
+
+			// random height
+			let index = Math.floor(Math.random(1) * greenShades.length);
+			let material = greenShades[index];
+			//console.log(material);
+
+			let height = Math.floor(Math.random(1) * 50);
+
+			let h = (50 + Math.floor(Math.random(1) * 50));
+			let w = (50 + Math.floor(Math.random(1) * 50));
+
+			let shape1 = new THREE.BoxGeometry(w,h,w);
+			let shape2 = new THREE.BoxGeometry(w-20,h+20,w-20);
+
+			let leaves1 = createShape(shape1, material, 0, 100 + height, 0);
+			tree.add(leaves1);
+
+			let leaves2 = createShape(shape2, material, 0, 100 + height, 0);
+			tree.add(leaves2);
 
 			tree.position.set((i*200-1000),0,(j*200-1000))
 			group.add(tree)
